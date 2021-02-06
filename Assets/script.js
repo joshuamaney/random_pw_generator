0a8e4414d4055a58df0e1543dcfdb7ab41b2f8a0
Joshua Maney <joshuaraymaney@gmail.com>
2021-02-05 22:27:55 -0500

    added var array

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,17 @@
 // Assignment Code
+
+// Array of options for computer to choose from
 var generateBtn = document.querySelector("#generate");
+
+var upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var numberOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var symbolOptions = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "{", "]", "}", ";", ":", ",", "<", ".", ">", "?"];
+
+
 
 // Write password to the #password input
 function writePassword() {
