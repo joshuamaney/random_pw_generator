ecff05119b2853ba820e3c1e49d4f63e891024a0
Joshua Maney <joshuaraymaney@gmail.com>
2021-02-06 00:02:36 -0500

    added random math and math floor variables for each of the variable options, fixed the script and style links in html

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,27 @@
 // Assignment Code
-
-// Array of options for computer to choose from
 var generateBtn = document.querySelector("#generate");
 
+// Array of options for computer to choose from
 var upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
 var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
 var numberOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharOptions = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "{", "]", "}", ";", ":", ",", "<", ".", ">", "?"];
 
-var symbolOptions = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "{", "]", "}", ";", ":", ",", "<", ".", ">", "?"];
+var upperCaseIndex = Math.floor(Math.random() * upperCaseOptions.length);
+var userChoiceUpper = upperCaseOptions[upperCaseIndex];
+console.log(userChoiceUpper);
+
+var lowerCaseIndex = Math.floor(Math.random() * lowerCaseOptions.length);
+var userChoiceLower = lowerCaseOptions[lowerCaseIndex];
+console.log(userChoiceLower);
+
+var numberIndex = Math.floor(Math.random() * numberOptions.length);
+var userChoiceNumber = numberOptions[numberIndex];
+console.log(userChoiceNumber);
+
+var specialCharIndex = Math.floor(Math.random() * specialCharOptions.length);
+var userChoiceSpecialChar = specialCharOptions[specialCharIndex];
+console.log(userChoiceSpecialChar);
 
 
 
